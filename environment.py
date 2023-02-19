52f05da423fa925f30adf09c771f2530831dde26
tnowak1502 <tnowak@mail.uni-mannheim.de>
2023-02-19 21:10:54 +0100

    custom model test, separated files

--- environment.py
+++ environment.py
@@ -1,7 +1,7 @@
 import argparse
 import gym
 from gym.core import ActType, ObsType
-from gym.spaces import Discrete, Box
+from gym.spaces import Discrete, Box, MultiDiscrete
 import numpy as np
 import os
 import random
@@ -23,169 +23,37 @@ from ray.tune.registry import get_trainable_cls
 tf1, tf, tfv = try_import_tf()
 torch, nn = try_import_torch()
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--run", type=str, default="PPO", help="The RLlib-registered algorithm to use."
-)
-parser.add_argument(
-    "--framework",
-    choices=["tf", "tf2", "torch"],
-    default="tf",
-    help="The DL framework specifier.",
-)
-parser.add_argument(
-    "--as-test",
-    action="store_true",
-    help="Whether this script should be run as a test: --stop-reward must "
-    "be achieved within --stop-timesteps AND --stop-iters.",
-)
-parser.add_argument(
-    "--stop-iters", type=int, default=50, help="Number of iterations to train."
-)
-parser.add_argument(
-    "--stop-timesteps", type=int, default=100000, help="Number of timesteps to train."
-)
-parser.add_argument(
-    "--stop-reward", type=float, default=0.1, help="Reward at which we stop training."
-)
-parser.add_argument(
-    "--no-tune",
-    action="store_true",
-    help="Run without Tune using a manual train loop instead. In this case,"
-    "use PPO without grid search and no TensorBoard.",
-)
-parser.add_argument(
-    "--local-mode",
-    action="store_true",
-    help="Init Ray in local mode for easier debugging.",
-)
-
 class AvalancheEnv(gym.Env):
     def __init__(self, config: EnvContext):
         self.action_space = Discrete(2)
         self.state = 0
+        self.flips = 0
+        self.num_steps = 0
+        self.needed_flips = config["needed_flips"]
         self.observation_space = Discrete(2)
 
     def step(self, action):
+        self.num_steps += 1
         done = False
+        reward = 0
         if action == 0:
             if self.state == 0:
                 self.state = 1
-                done = True
+                self.flips += 1
         else:
             if self.state == 1:
                 self.state = 0
-                done = True
-        return self.state, random.random() * 2 if done else -0.1, done, {}
+                self.flips += 1
+        #print("flips:", self.flips)
+        if self.flips >= self.needed_flips:
+            done = True
+        if done:
+            reward = (self.needed_flips+1)-self.num_steps
+        #print("reward", reward)
+        return self.state, reward, done, {}
 
     def reset(self):
         self.state = 0
+        self.flips = 0
+        self.num_steps = 0
         return self.state
-    
-class TorchCustomModel(TorchModelV2, nn.Module):
-    """Example of a PyTorch custom model that just delegates to a fc-net."""
-
-    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
-        TorchModelV2.__init__(
-            self, obs_space, action_space, num_outputs, model_config, name
-        )
-        nn.Module.__init__(self)
-
-        self.torch_sub_model = TorchFC(
-            obs_space, action_space, num_outputs, model_config, name
-        )
-
-    def forward(self, input_dict, state, seq_lens):
-        input_dict["obs"] = input_dict["obs"].float()
-        fc_out, _ = self.torch_sub_model(input_dict, state, seq_lens)
-        return fc_out, []
-
-    def value_function(self):
-        return torch.reshape(self.torch_sub_model.value_function(), [-1])
-
-class CustomModel(TFModelV2):
-    """Example of a keras custom model that just delegates to an fc-net."""
-
-    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
-        super(CustomModel, self).__init__(
-            obs_space, action_space, num_outputs, model_config, name
-        )
-        self.model = FullyConnectedNetwork(
-            obs_space, action_space, num_outputs, model_config, name
-        )
-
-    def forward(self, input_dict, state, seq_lens):
-        return self.model.forward(input_dict, state, seq_lens)
-
-    def value_function(self):
-        return self.model.value_function()
-
-if __name__ == "__main__":
-    args = parser.parse_args()
-    print(f"Running with following CLI options: {args}")
-
-    ray.init(local_mode=args.local_mode)
-
-    # Can also register the env creator function explicitly with:
-    # register_env("corridor", lambda config: AvalancheEnv(config))
-    ModelCatalog.register_custom_model(
-        "my_model", TorchCustomModel if args.framework == "torch" else CustomModel
-    )
-
-    config = (
-        get_trainable_cls(args.run)
-        .get_default_config()
-        # or "corridor" if registered above
-        .environment(AvalancheEnv, env_config={"corridor_length": 5})
-        .framework(args.framework)
-        .rollouts(num_rollout_workers=1)
-        .training(
-            model={
-                "custom_model": "my_model",
-                "vf_share_layers": True,
-            }
-        )
-        # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-        .resources(num_gpus=int(os.environ.get("RLLIB_NUM_GPUS", "0")))
-    )
-
-    stop = {
-        "training_iteration": args.stop_iters,
-        "timesteps_total": args.stop_timesteps,
-        "episode_reward_mean": args.stop_reward,
-    }
-
-    if args.no_tune:
-        # manual training with train loop using PPO and fixed learning rate
-        if args.run != "PPO":
-            raise ValueError("Only support --run PPO with --no-tune.")
-        print("Running manual train loop without Ray Tune.")
-        # use fixed learning rate instead of grid search (needs tune)
-        config.lr = 1e-3
-        algo = config.build()
-        # run manual training loop and print results after each iteration
-        for _ in range(args.stop_iters):
-            result = algo.train()
-            print(pretty_print(result))
-            # stop training of the target train steps or reward are reached
-            if (
-                result["timesteps_total"] >= args.stop_timesteps
-                or result["episode_reward_mean"] >= args.stop_reward
-            ):
-                break
-        algo.stop()
-    else:
-        # automated run with Tune and grid search and TensorBoard
-        print("Training automatically with Ray Tune")
-        tuner = tune.Tuner(
-            args.run,
-            param_space=config.to_dict(),
-            run_config=air.RunConfig(stop=stop),
-        )
-        results = tuner.fit()
-
-        if args.as_test:
-            print("Checking if learning goals were achieved")
-            check_learning_achieved(results, args.stop_reward)
-
-    ray.shutdown()
