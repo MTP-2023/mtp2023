a732c55a049018d967b07f2d51e97e7ad3963f4a
tnowak1502 <tnowak@mail.uni-mannheim.de>
2023-03-02 09:58:31 +0100

    changed file structure, prebuilt environment test

---
+++ main/model.py
@@ -0,0 +1,100 @@
+import argparse
+import gym
+from gym.core import ActType, ObsType
+from gym.spaces import Discrete, Box
+import numpy as np
+import os
+import random
+
+from ray.rllib.env import EnvContext
+import ray
+from ray import air, tune
+from ray.rllib.env.env_context import EnvContext
+from ray.rllib.models import ModelCatalog
+from ray.rllib.models.tf.tf_modelv2 import TFModelV2
+from ray.rllib.models.tf.fcnet import FullyConnectedNetwork
+from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
+from ray.rllib.utils.framework import try_import_tf, try_import_torch
+from ray.rllib.utils.test_utils import check_learning_achieved
+from ray.tune.logger import pretty_print
+from ray.tune.registry import get_trainable_cls
+from ray.rllib.models.tf.misc import normc_initializer
+
+tf1, tf, tfv = try_import_tf()
+torch, nn = try_import_torch()
+
+#from https://github.com/ray-project/ray/blob/releases/2.2.0/rllib/examples/custom_keras_model.py
+class CustomModel(TFModelV2):
+
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+        #print("Keras model called")
+        super(CustomModel, self).__init__(
+            obs_space, action_space, num_outputs, model_config, name
+        )
+        self.inputs = tf.keras.layers.Input(shape=obs_space.shape, name="observations")
+        layer_1 = tf.keras.layers.Dense(
+            128,
+            name="my_layer1",
+            activation=tf.nn.relu,
+            kernel_initializer=normc_initializer(1.0),
+        )(self.inputs)
+        layer_out = tf.keras.layers.Dense(
+            num_outputs,
+            name="my_out",
+            activation=None,
+            kernel_initializer=normc_initializer(0.01),
+        )(layer_1)
+        value_out = tf.keras.layers.Dense(
+            1,
+            name="value_out",
+            activation=None,
+            kernel_initializer=normc_initializer(0.01),
+        )(layer_1)
+        self.base_model = tf.keras.Model(self.inputs, [layer_out, value_out])
+
+    def forward(self, input_dict, state, seq_lens):
+        model_out, self._value_out = self.base_model(input_dict["obs"])
+        return model_out, state
+
+    def value_function(self):
+        return tf.reshape(self._value_out, [-1])
+
+class TorchCustomModel(TorchModelV2, nn.Module):
+    """Example of a PyTorch custom model that just delegates to a fc-net."""
+
+    def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+        TorchModelV2.__init__(
+            self, obs_space, action_space, num_outputs, model_config, name
+        )
+        nn.Module.__init__(self)
+
+        self.torch_sub_model = TorchFC(
+            obs_space, action_space, num_outputs, model_config, name
+        )
+
+    def forward(self, input_dict, state, seq_lens):
+        input_dict["obs"] = input_dict["obs"].float()
+        fc_out, _ = self.torch_sub_model(input_dict, state, seq_lens)
+        return fc_out, []
+
+    def value_function(self):
+        return torch.reshape(self.torch_sub_model.value_function(), [-1])
+
+#original example version
+# class CustomModel(TFModelV2):
+#     """Example of a keras custom model that just delegates to an fc-net."""
+#
+#     def __init__(self, obs_space, action_space, num_outputs, model_config, name):
+#         super(CustomModel, self).__init__(
+#             obs_space, action_space, num_outputs, model_config, name
+#         )
+#         self.model = FullyConnectedNetwork(
+#             obs_space, action_space, num_outputs, model_config, name
+#         )
+#
+#     def forward(self, input_dict, state, seq_lens):
+#         return self.model.forward(input_dict, state, seq_lens)
+#
+#     def value_function(self):
+#         return self.model.value_function()
