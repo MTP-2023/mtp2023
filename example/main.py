2f4a9b732a03a975d084e759bee0701a8c389ed1
tnowak1502 <tnowak@mail.uni-mannheim.de>
2023-03-02 11:13:30 +0100

    semi-live visualization of example, changed results directory, changed arguments from command line to class

--- example/main.py
+++ example/main.py
@@ -19,6 +19,9 @@ from ray.rllib.utils.framework import try_import_tf, try_import_torch
 from ray.rllib.utils.test_utils import check_learning_achieved
 from ray.tune.logger import pretty_print
 from ray.tune.registry import get_trainable_cls
+from ray.rllib.algorithms.ppo import PPOConfig
+import warnings
+
 
 tf1, tf, tfv = try_import_tf()
 torch, nn = try_import_torch()
@@ -26,48 +29,27 @@ torch, nn = try_import_torch()
 from environment import AvalancheEnv
 from model import TorchCustomModel, CustomModel
 
-parser = argparse.ArgumentParser()
-parser.add_argument(
-    "--run", type=str, default="PPO", help="The RLlib-registered algorithm to use."
-)
-parser.add_argument(
-    "--framework",
-    choices=["tf", "tf2", "torch"],
-    default="tf",
-    help="The DL framework specifier.",
-)
-parser.add_argument(
-    "--as-test",
-    action="store_true",
-    help="Whether this script should be run as a test: --stop-reward must "
-    "be achieved within --stop-timesteps AND --stop-iters.",
-)
-parser.add_argument(
-    "--stop-iters", type=int, default=50, help="Number of iterations to train."
-)
-parser.add_argument(
-    "--stop-timesteps", type=int, default=100000, help="Number of timesteps to train."
-)
-parser.add_argument(
-    "--stop-reward", type=float, default=1, help="Reward at which we stop training."
-)
-parser.add_argument(
-    "--no-tune",
-    action="store_true",
-    help="Run without Tune using a manual train loop instead. In this case,"
-    "use PPO without grid search and no TensorBoard.",
-)
-parser.add_argument(
-    "--local-mode",
-    action="store_true",
-    help="Init Ray in local mode for easier debugging.",
-)
+class Arguments():
+    # default values
+    def __init__(self) -> None:
+        self.run = "PPO"
+        self.framework = "tf"
+        self.as_test = "store_true"
+        self.stop_iters = 50
+        self.stop_timesteps = 100000
+        self.stop_reward = 200
+        self.no_tune = "store_true"
+        self.local_mode = "store_true"
+
+args = Arguments()
+warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 if __name__ == "__main__":
-    args = parser.parse_args()
+    #args = parser.parse_args()
+
     print(f"Running with following CLI options: {args}")
 
-    ray.init(local_mode=args.local_mode)
+    ray.init(local_mode=args.local_mode, num_gpus=1)
 
     # Can also register the env creator function explicitly with:
     # register_env("corridor", lambda config: AvalancheEnv(config))
@@ -75,23 +57,39 @@ if __name__ == "__main__":
         "my_model", TorchCustomModel if args.framework == "torch" else CustomModel
     )
 
-    env = gym.make("CartPole-v1")
-
     config = (
         get_trainable_cls(args.run)
-        .get_default_config()
-        # or "corridor" if registered above
-        .environment(env)
-        .framework(args.framework)
-        .rollouts(num_rollout_workers=1)
-        .training(
-            model={
-                "custom_model": "my_model",
-                "vf_share_layers": True,
-            }
+            .get_default_config()
+            # or "corridor" if registered above
+            .environment(env="CartPole-v1")#, render_env=True)
+            .framework(args.framework)
+            .rollouts(num_rollout_workers=1)
+            .training(
+                model={
+                    "custom_model": "my_model",
+                    "vf_share_layers": True,
+                }
+            )
+            .evaluation(
+            # Evaluate once per training iteration.
+            evaluation_interval=1,
+            # Run evaluation on (at least) two episodes
+            evaluation_duration=2,
+            # ... using one evaluation worker (setting this to 0 will cause
+            # evaluation to run on the local evaluation worker, blocking
+            # training until evaluation is done).
+            evaluation_num_workers=1,
+            # Special evaluation config. Keys specified here will override
+            # the same keys in the main config, but only for evaluation.
+            evaluation_config={
+                # Render the env while evaluating.
+                # Note that this will always only render the 1st RolloutWorker's
+                # env and only the 1st sub-env in a vectorized env.
+                "render_env": True,
+            },
         )
-        # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
-        .resources(num_gpus=int(os.environ.get("RLLIB_NUM_GPUS", "0")))
+            # Use GPUs iff `RLLIB_NUM_GPUS` env var set to > 0.
+            .resources(num_gpus=int(os.environ.get("RLLIB_NUM_GPUS", "0")))
     )
 
     stop = {
@@ -114,8 +112,8 @@ if __name__ == "__main__":
             print(pretty_print(result))
             # stop training of the target train steps or reward are reached
             if (
-                result["timesteps_total"] >= args.stop_timesteps
-                or result["episode_reward_mean"] >= args.stop_reward
+                    result["timesteps_total"] >= args.stop_timesteps
+                    or result["episode_reward_mean"] >= args.stop_reward
             ):
                 break
         algo.stop()
@@ -125,7 +123,7 @@ if __name__ == "__main__":
         tuner = tune.Tuner(
             args.run,
             param_space=config.to_dict(),
-            run_config=air.RunConfig(stop=stop),
+            run_config=air.RunConfig(stop=stop, local_dir="./results", name="test_experiment"),
         )
         results = tuner.fit()
 
