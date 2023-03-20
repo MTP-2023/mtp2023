362e095097b6228ad8c02888b2d5e47eb15a699c
mtemnov <michael.temnov@students.uni-mannheim.de>
2023-03-20 13:57:58 +0100

    fix bugs in refactoring of generation method

--- gameEnv/boardGenerator/generate.py
+++ gameEnv/boardGenerator/generate.py
@@ -1,4 +1,5 @@
 import random
+import itertools
 
 def add_random_switch():
     switch = [0, 0]
@@ -6,16 +7,20 @@ def add_random_switch():
     switch[r] = 1
     return switch
 
-def generate_random_board(width, height):
+def generate_random_board(switches, row_pairs):
+    #width = 2 + (2 * switches)
+    height = 2 * row_pairs
     board = []
     for i in range(height):
-        row = []
         if i%2 == 0:
+            row = []
             row.append(0)
-            row.append([add_random_switch() for _ in range(width-1)])
+            row += list(itertools.chain(*[add_random_switch() for _ in range(switches)]))
             row.append(0)
+            print(row)
         else:
-            row = [add_random_switch() for _ in range(width)]
+            row = list(itertools.chain(*[add_random_switch() for _ in range(switches+1)]))
+            print(row)
         board.append(row)
    
     return board
