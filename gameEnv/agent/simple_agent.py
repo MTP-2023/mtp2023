4633011024ec9a19b13ede784834afde044f1654
RezaXsys <sakura.7@icloud.com>
2023-03-21 09:59:23 +0200

    simple agent continue

--- gameEnv/agent/simple_agent.py
+++ gameEnv/agent/simple_agent.py
@@ -2,6 +2,7 @@ import json
 from gameEnv.simulation.simulate import run
 import copy
 
+
 class SimpleAgent:
 
     def __init__(self):
@@ -13,19 +14,43 @@ class SimpleAgent:
         self.height = data['height']
         self.width = data['width']
 
+    def heuristic_simple(self, board):
+        correctmarbles = 0
+        goalmarbles = 0
+        winpercentage = 0.0
+        i = 0
+        while i < self.height:
+            j = 0
+            if i % 2 == 0:
+                j = 1
+                while j < self.width - 1:
+                    if self.endboard[i][j] == 2 or self.endboard[i][j + 1] == 2:
+                        goalmarbles += 1
+                        if self.startboard[i][j] == 2 or self.startboard[i][j + 1] == 2:
+                            correctmarbles += 1
 
-    def heuristic(self, board):
-        pass
+                    j += 2
+
+            i += 1
+
+        winpercentage = correctmarbles / goalmarbles
+
+        dictA = {'correctmarbles': correctmarbles, 'goalmarbles': goalmarbles, 'winpercentage': winpercentage}
+        return dictA
+
     def step(self, board):
-
-        for i in range(self.width*2):
+        maxreward = 0
+        bestmove = 0
+        for i in range(self.width * 2):
             result = run(i, copy.deepcopy(board), True)
-            #heuristic(self,result)
+            current = self.heuristic_simple(self, result)["correctmarbles"]
 
+            if current > maxreward:
+                maxreward = current
+                bestmove = i
 
-        pass
-
+        return bestmove
 
     if __name__ == "__main__":
+
         pass
-    
