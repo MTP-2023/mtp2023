87a18f2dde2c4a653d2e1a7ad37e1ddd1e339899
tnowak1502 <77999292+tnowak1502@users.noreply.github.com>
2023-04-21 17:00:42 +0200

    new train and test set for curriculum learning

--- gameResources/challengeGenerator/generateChallenges.py
+++ gameResources/challengeGenerator/generateChallenges.py
@@ -7,8 +7,8 @@ import json
 if __name__ == "__main__":
     width = 3
     height = 2
-    minMarbles = 0
-    maxMarbles = 100
+    minMarbles = 1
+    maxMarbles = 2
     dict = {
         "width": width,
         "height": height,
@@ -17,7 +17,7 @@ if __name__ == "__main__":
     turns = [16, 14, 12, 10, 8, 6, 4]
     for max_turns in turns:
         training_states = []
-        for i in range(100):
+        for i in range(500):
             randomBoard = generate_random_board(width, height)
             print("Generating boards with max_turns ", max_turns)
             goal = generateGoalState(randomBoard, minMarbles, maxMarbles, max_turns, 42, width*2, False)
@@ -27,5 +27,5 @@ if __name__ == "__main__":
         dict["training_levels"].append(training_states)
     json_object = json.dumps(dict, indent=4)
 
-    with open("../../gameVariants/baseline/training/curriculumVer1" + ".json", "w") as outfile:
+    with open("../../gameVariants/baseline/training/curriculumVer2Test" + ".json", "w") as outfile:
         outfile.write(json_object)
