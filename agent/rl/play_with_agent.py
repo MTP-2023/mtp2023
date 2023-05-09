4c3bcfa0421570de47f3d20fcbcf004f0436e0d6
mtemnov <michael.temnov@students.uni-mannheim.de>
2023-05-09 17:42:22 +0200

    push play with agent status from apply_agent

--- agent/rl/play_with_agent.py
+++ agent/rl/play_with_agent.py
@@ -1,74 +1,74 @@
 import json
 import numpy as np
 from ray.rllib.policy.policy import Policy
-from ray.rllib.algorithms.algorithm import Algorithm
-import tensorflow as tf
-from ray.rllib.algorithms.ppo import PPOConfig
 import sys
+from ray.air.integrations.wandb import setup_wandb
 
 sys.path.append("../../")
 from gameVariants.baseline.reward import reward
 from gameResources.simulation.simulate import run
-from agent.rl.train_resources.avalancheEnv import GameBoardEnv
-from ray.rllib.models import ModelCatalog
 from collections import OrderedDict
+from apply_policy import return_move
 
-# policy = Policy.from_checkpoint(
-#     "C:/Users/thoma/PycharmProjects/mtp2023/gameEnv/rllibEnv/results/PPO_no_model_gen_test2_new_rewards_big_grid_search/PPO_GameBoardEnv_fc999_00006_6_clip_param=0.3000,lr=0.0002_2023-03-21_19-07-29/checkpoint_000300/policies/default_policy")
+run_wandb = setup_wandb(api_key_file="wandb_api_key.txt")
+artifact = run_wandb.use_artifact('mtp2023_avalanche/CurriculumLearning/checkpoint_stop125_thresh65_new_set_ray_curr:v130', type='model')
+artifact_dir = artifact.download()
 
-def return_move(artifact_dir, obs):
-    # load policy
-    agent = Policy.from_checkpoint(artifact_dir)
+agent = Policy.from_checkpoint(artifact_dir+'/policies/default_policy')
 
-    # create "empty" env to obtain preprocessor
-    preprocessor = ModelCatalog.get_preprocessor(GameBoardEnv(config={}, example_board=challenge.current))
+class environment:
+    def __init__(self, current_board, goal_board, n_steps, max_steps, width, height):
+        self.current_board = current_board
+        self.goal_board = goal_board
+        self.n_steps = n_steps
+        self.max_steps = max_steps
+        self.width = width
+        self.height = height
 
-    # flatten obs and query results
-    flat_obs = preprocessor.transform(obs)
-    move = agent.compute_single_action(flat_obs)
-
-    # returns action
-    return move[0]
-
-agent = Algorithm.from_checkpoint("C:/Users/thoma/PycharmProjects/mtp2023/gameEnv/rllibEnv/results/PPO_no_model_gen_test2_new_rewards_big_grid_search/PPO_GameBoardEnv_fc999_00006_6_clip_param=0.3000,lr=0.0002_2023-03-21_19-07-29/checkpoint_000300")
-
-challenges = json.load(open("../../gameVariants/baseline/training/generationTest2.json"))
+challenges = json.load(open("../../gameVariants/baseline/training/curriculumVer2.json"))
 height = challenges["height"] * 2
 width = challenges["width"] * 2 + 2
-noOfChallenges = len(challenges["training_states"])
-print(noOfChallenges)
+noOfLevels = len(challenges["training_levels"])
+print(noOfLevels)
 solvedChallenges = 0
 totalTurns = 0
+noOfChallenges = 0
 
-for j in range(noOfChallenges):
-    print("ATTEMPTING CHALLENGE", j)
-    challenge = challenges["training_states"][j]
-    current_board = np.array(challenge["start_board"])
-    goal_board = np.array(challenge["goal_board"])
-    max_steps = challenge["max_turns"]
 
-    print("START BOARD\n", current_board)
-    print("GOAL BOARD\n", goal_board)
-    done = False
-    for i in range(max_steps):
-        obs = OrderedDict()
-        obs["current"] = current_board
-        obs["goal"] = goal_board
-        move = agent.compute_single_action(obs)
-        print("TURN", i, "SELECTED MOVE:", move)
-        run(move, current_board)
-        print("UPDATED BOARD\n", current_board)
-        reward, done = reward(i, max_steps, height, width, goal_board, current_board)
-        if done:
-            print("FINISHED CHALLENGE IN", i, "TURNS\n")
-            solvedChallenges += 1
-            totalTurns += i
-            break
-    if not done:
-        #totalTurns += max_steps+1
-        print("FAILED CHALLENGE :((((\n")
-
+for k in range(noOfLevels):
+    noOfChallengesLvl = len(challenges["training_levels"][k])
+    noOfChallenges += noOfChallengesLvl
+    print("LEVEL", k)
+    for j in range(noOfChallengesLvl):
+        print("ATTEMPTING CHALLENGE", j)
+        challenge = challenges["training_levels"][k][j]
+        current_board = np.array(challenge["start_board"])
+        goal_board = np.array(challenge["goal_board"])
+        max_steps = challenge["max_turns"]
+        print("START BOARD\n", current_board)
+        print("GOAL BOARD\n", goal_board)
+        done = False
+        for i in range(max_steps):
+            obs = OrderedDict()
+            obs["current"] = current_board
+            obs["goal"] = goal_board
+            move = return_move(agent, obs)
+            print("TURN", i, "SELECTED MOVE:", move)
+            run(move, current_board)
+            print("UPDATED BOARD\n", current_board)
+            envObj = environment(current_board, goal_board, i, max_steps, width, height)
+            x, done = reward(envObj)
+            if done:
+                print("FINISHED CHALLENGE IN", i, "TURNS\n")
+                solvedChallenges += 1
+                totalTurns += i
+                break
+        if not done:
+            #totalTurns += max_steps+1
+            print("FAILED CHALLENGE :((((\n")
 solvedPercent = float(solvedChallenges)/float(noOfChallenges)
 print("SOLVE RATE:", solvedPercent)
 avgTurns = float(totalTurns)/float(solvedChallenges)
 print("AVERAGE TURNS:", avgTurns)
+
+run_wandb.finish()
