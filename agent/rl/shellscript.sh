e0aa1e4824a82374abfbbc20a6bca4e5c7bde4fa
tnowak1502 <77999292+tnowak1502@users.noreply.github.com>
2023-05-19 14:07:23 +0200

    small fix

--- agent/rl/shellscript.sh
+++ agent/rl/shellscript.sh
@@ -1,2 +1,2 @@
 #!/bin/sh 
-python -u ./train.py --train_on curriculumVer2 --results_folder gpu_testtt --stop_reward 13 --curriculum_threshold 6.5 --num_cpus 0 --num_gpus 2 --log_as gpu_testt --variant baseline --curriculum ray
+python -u ./train.py --train_on curriculumVer2 --results_folder gpu_testtt --stop_reward 13 --curriculum_threshold 6.5 --num_cpus 2 --num_gpus 2 --log_as gpu_testt --variant baseline --curriculum ray
