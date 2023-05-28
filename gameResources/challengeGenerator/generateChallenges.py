6c06bdbfd975b76e5e2de212ea33f70c16a671f3
tnowak1502 <77999292+tnowak1502@users.noreply.github.com>
2023-05-28 11:53:52 +0200

    new train/test set

--- gameResources/challengeGenerator/generateChallenges.py
+++ gameResources/challengeGenerator/generateChallenges.py
@@ -6,15 +6,24 @@ import json
 
 def check_existing(states, start, goal):
     for j in range(len(states)):
-        if states[j]["startboard"] == start and states[j]["goal_board"] == goal:
+        if states[j]["start_board"] == start and states[j]["goal_board"] == goal:
             return True
+    return False
+
+def check_train(train_set, start, goal):
+    for level in train_set["training_levels"]:
+        for challenge in level:
+            if challenge["start_board"] == start:
+                if challenge["goal_board"] == goal:
+                    return True
     return False
 
 if __name__ == "__main__":
     width = 3
     height = 2
-    minMarbles = 1
+    minMarbles = 2
     maxMarbles = 2
+    train_set = json.load(open("../../gameVariants/baseline/training/curriculum2Marbles" + ".json"))
     dict = {
         "width": width,
         "height": height,
@@ -31,7 +40,7 @@ if __name__ == "__main__":
             goal = generateGoalState(randomBoard, minMarbles, maxMarbles, max_turns, 42, width*2, False)
             print(randomBoard)
             print(goal)
-            while check_existing(training_states, randomBoard, goal):
+            while check_existing(training_states, randomBoard, goal) or check_train(train_set, randomBoard, goal):
                 randomBoard = generate_random_board(width, height)
                 print("Generating boards with max_turns ", max_turns)
                 goal = generateGoalState(randomBoard, minMarbles, maxMarbles, max_turns, 42, width * 2, False)
@@ -43,5 +52,5 @@ if __name__ == "__main__":
         dict["training_levels"].append(training_states)
     json_object = json.dumps(dict, indent=4)
 
-    with open("../../gameVariants/baseline/training/curriculumVer2Test" + ".json", "w") as outfile:
+    with open("../../gameVariants/baseline/training/curriculum2MarblesTest" + ".json", "w") as outfile:
         outfile.write(json_object)
