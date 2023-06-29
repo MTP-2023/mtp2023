c8fbaa93a0a5db18fd67d9de54ba689cb2d2bfee
tnowak1502 <77999292+tnowak1502@users.noreply.github.com>
2023-06-29 12:09:53 +0200

    more fixes

--- SecondHalf/agent/rl/train_resources/multiplayerEnv.py
+++ SecondHalf/agent/rl/train_resources/multiplayerEnv.py
@@ -14,7 +14,7 @@ class MultiplayerEnv(GameBoardEnv):
         super().__init__(config, level, challenge_idx, train)
         self.observation_space = Dict({
             "current": Box(low=-2, high=2, shape=(self.height, self.width), dtype=int),
-            "goal": Box(low=-2, high=2, shape=(self.height, self.width), dtype=int)
+            "goal": Box(low=-2, high=3, shape=(self.height, self.width), dtype=int)
         })
         self.current_player = 1
         self.vs = config.get("vs", "random")
@@ -27,11 +27,16 @@ class MultiplayerEnv(GameBoardEnv):
         #if self.n_steps == 0:
         #    print("GOAL\n", self.goal_board)
         #print("ACTION", action)
-        self.current_board = run(action, self.current_board, self.current_player)
-        #print(self.current_board)
-        self.n_steps += 1
 
+        #check if last move of other player won the game for current player
         reward, done = self.reward_module.reward(self)
+
+        if not done:
+            self.current_board = run(action, self.current_board, self.current_player)
+            #print(self.current_board)
+            self.n_steps += 1
+
+            reward, done = self.reward_module.reward(self)
 
         if done:
             obs = {
@@ -40,30 +45,35 @@ class MultiplayerEnv(GameBoardEnv):
             }
 
             """print("DONE ON AGENT TURN")
+            print(self.n_steps)
             print(self.current_board)
             print(self.goal_board)
             print(reward)"""
             return obs, reward, done, False, {}
         else:
             self.current_player = -1
-            if self.vs == "mcts":
-                enemyAction = mcts(self.current_board, 5, 1, self.goal_board, self.width-2, self.height, self.max_steps, self.n_steps, self.current_player)
-            else:
-                enemyAction = random.randint(0, self.n_choices)
-            #print("ENEMY ACTION", enemyAction)
-            self.current_board = run(enemyAction, self.current_board, self.current_player)
-            #print("ENEMY ACTION", enemyAction)
-            #print(self.current_board)
-
             reward, done = self.reward_module.reward(self)
+            if not done:
+                if self.vs == "mcts":
+                    enemyAction = mcts(self.current_board, 5, 1, self.goal_board, self.width-2, self.height, self.max_steps, self.n_steps, self.current_player)
+                else:
+                    enemyAction = random.randint(0, self.n_choices)
+                #print("ENEMY ACTION", enemyAction)
+                self.current_board = run(enemyAction, self.current_board, self.current_player)
+                #print("ENEMY ACTION", enemyAction)
+                #print(self.current_board)
+
+                reward, done = self.reward_module.reward(self)
             self.current_player = 1
             obs = {
                 "current": self.current_board,
                 "goal": self.goal_board
             }
 
-            """if done:
-                print("DONE ON ENEMY TURN")
+            if done:
+                """print("DONE ON ENEMY TURN")
+                print(self.n_steps)
+                print(self.current_board)
                 print(self.goal_board)
                 print(reward)"""
             return obs, reward, done, False, {}
