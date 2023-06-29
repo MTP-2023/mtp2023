12a89957bcf968e1fe9fdc0788b2c753e3825844
tnowak1502 <77999292+tnowak1502@users.noreply.github.com>
2023-06-29 15:27:01 +0200

    fix multiplayer challenge generation

--- SecondHalf/gameResources/challengeGenerator/generateChallenges.py
+++ SecondHalf/gameResources/challengeGenerator/generateChallenges.py
@@ -20,19 +20,37 @@ def check_train(train_set, start, goal):
 
 
 def merge(goal, goal2, width, height):
+    #print(goal)
+    #print(goal2)
+    width = width*2+2
     result = copy.deepcopy(goal)
     for i in range(height * 2):
-        uneven = 0
-        if i % 2 != 0:
-            uneven = 1
+        #print("i",i)
         j = 0
-        while j < (width - uneven) * 2:
-            if goal2[i][j] == 2 or goal2[i][j+1] == 2:
-                if goal[i][j] == 2 or goal[i][j + 1] == 2:
+        if i % 2 == 0:
+            j = 1
+        while j < width - 1:
+            #print("j",j)
+            if goal2[i][j] == 2:
+                if goal[i][j] == 2:
                     result[i][j] = 3
-                else:
+                elif goal[i][j+1] == 2:
+                    result[i][j+1] = 3
+                elif goal[i][j] == 1:
                     result[i][j] = -2
+                elif goal[i][j+1] == 1:
+                    result[i][j+1] = -2
+            elif goal2[i][j+1] == 2:
+                if goal[i][j] == 2:
+                    result[i][j] = 3
+                elif goal[i][j+1] == 2:
+                    result[i][j+1] = 3
+                elif goal[i][j] == 1:
+                    result[i][j] = -2
+                elif goal[i][j+1] == 1:
+                    result[i][j+1] = -2
             j += 2
+    #(result)
     return result
 
 
@@ -54,35 +72,34 @@ if __name__ == "__main__":
 
     turns = [16, 14, 12, 10, 8, 6, 4]
     for max_turns in turns:
-        starts = []
-        goals = []
         training_states = []
         for i in range(500):
             randomBoard = generate_random_board(width, height)
             print("Generating boards with max_turns ", max_turns)
             goal = generateGoalState(randomBoard, minMarbles, maxMarbles, max_turns, 42, width*2, False)
             goal2 = generateGoalState(randomBoard, minMarbles, maxMarbles, max_turns, 42, width*2, False)
-            print(randomBoard)
-            print(goal)
-            print(goal2)
+            #print(randomBoard)
+            #print("GOAL  ", goal)
+            #print("GOAL2 ", goal2)
             merged_goal = merge(goal, goal2, width, height)
+            #print("MERGED", merged_goal)
             while check_existing(training_states, randomBoard, merged_goal):
                 #or check_train(train_set, randomBoard, merged_goal):
                 randomBoard = generate_random_board(width, height)
                 print("Generating boards with max_turns ", max_turns)
                 goal = generateGoalState(randomBoard, minMarbles, maxMarbles, max_turns, 42, width * 2, False)
                 goal2 = generateGoalState(randomBoard, minMarbles, maxMarbles, max_turns, 42, width * 2, False)
-                print(randomBoard)
-                print(goal)
-                print(goal2)
+                #print(randomBoard)
+                #print(goal)
+                #print(goal2)
                 merged_goal = merge(goal, goal2, width, height)
-            print(merged_goal)
-            goals.append(merged_goal)
-            starts.append(randomBoard)
-            training_states.append({"start_board": randomBoard, "goal_board": goal, "max_turns": max_turns})
+            #print(merged_goal)
+            training_states.append({"start_board": randomBoard, "goal_board": merged_goal, "max_turns": max_turns})
         dict["training_levels"].append(training_states)
+        #print()
+        #print(dict)
     json_object = json.dumps(dict, indent=4)
 
-    with open("../../gameVariants/baseline/training/multiplayer_train" + ".json", "w") as outfile:
+    with open("../../gameVariants/multiplayer/training/multiplayer_train" + ".json", "w") as outfile:
         outfile.write(json_object)
 
