f01053215f40043b26a55bc17256e362a6e433bc
tnowak1502 <77999292+tnowak1502@users.noreply.github.com>
2023-07-10 19:03:03 +0200

    agent can play full challenge sets

--- SecondHalf/agent/rl/play_vs_agent.py
+++ SecondHalf/agent/rl/play_vs_agent.py
@@ -70,98 +70,158 @@ parser.add_argument(
     default="10"
 )
 
+parser.add_argument(
+    "--challenges",
+    default="random"
+)
+
 args = parser.parse_args()
 
-#run_wandb = setup_wandb(api_key_file="wandb_api_key.txt")
-#artifact = run_wandb.use_artifact('mtp2023_avalanche/CurriculumVer2Fix/checkpoint_multiplayer_fr_vs_mcts100:v3', type='model')
-artifact_dir = "./artifacts/checkpoint_multiplayer_fr_vs_mcts100-v3"#artifact.download()
+run_wandb = setup_wandb(api_key_file="wandb_api_key.txt")
+artifact = run_wandb.use_artifact('mtp2023_avalanche/CurriculumVer2Fix/checkpoint_random1marblewinrate:v99', type='model')
+artifact_dir = artifact.download()
 
 agent = Policy.from_checkpoint(artifact_dir+'/policies/default_policy')
 
-width = 3
-height = 2
-minMarbles = 2
-maxMarbles = 2
-max_turns = 16
-current_board = generate_random_board(width, height)
-goal = generateGoalState(current_board, minMarbles, maxMarbles, max_turns, 42, width*2, False)
-goal2 = generateGoalState(current_board, minMarbles, maxMarbles, max_turns, 42, width*2, False)
-goal_board = merge(goal, goal2, width, height)
+if args.challenges == "random":
+    width = 3
+    height = 2
+    minMarbles = 1
+    maxMarbles = 1
+    max_turns = 16
+    noOfLevels = 1
+    current_board = generate_random_board(width, height)
+    goal = generateGoalState(current_board, minMarbles, maxMarbles, max_turns, 42, width*2, False)
+    goal2 = generateGoalState(current_board, minMarbles, maxMarbles, max_turns, 42, width*2, False)
+    goal_board = merge(goal, goal2, width, height)
+    training_levels = []
+    training_levels.append({"start_board": current_board, "goal_board": goal_board, "max_turns": max_turns})
 
-obs = OrderedDict()
-obs["current"] = current_board
-obs["goal"] = goal_board
+else:
+    challenges = json.load(open("../../gameVariants/multiplayer/training/" + args.challenges + ".json"))
+    height = challenges["height"]
+    width = challenges["width"]
+    training_levels = challenges["training_levels"]
+    noOfLevels = len(training_levels)
 
-done = False
-step = 0
-print("      GOAL BOARD")
-print_board(goal_board)
-print("      START BOARD")
-print_board(current_board)
-while step < max_turns:
-    print("      STEP", step)
-    player = 1
-    solveEnv = ShallowEnv(current_board, obs["goal"], step + 1, max_turns, len(current_board[0]), len(current_board),
-                          player)
-    # determine if goal is fulfilled
-    _, done = reward(solveEnv)
-    if done:
-        if _ > 0:
-            print("     ", args.player1, "WON")
-        else:
-            print("     ", args.player2, " WON")
-    if args.player1 == "agent":
+wonChallenges = 0
+totalTurns = 0
+noOfChallenges = 0
+levelStats = []
+
+for leveli in range(noOfLevels):
+    level = training_levels[leveli]
+    noOfChallengesLvl = len(level)
+    noOfChallenges += noOfChallengesLvl
+    wonChallengesLvl = 0
+    totalTurnsLvl = 0
+    for challengei in range(noOfChallengesLvl):
+        challenge = level[challengei]
+
+        current_board = challenge["start_board"]
+        goal_board = challenge["goal_board"]
+        max_turns = challenge["max_turns"]
+        obs = OrderedDict()
         obs["current"] = current_board
-        paramEnv = ShallowEnv(current_board, obs["goal"], step, max_turns, len(current_board[0]), len(current_board), player)
-        action = return_move(agent, paramEnv, obs)
-    elif args.player1 == "mcts":
-        action = mcts(current_board, int(args.mcts_depth), math.sqrt(2), goal_board, width * 2, height, max_turns, step, player)
-    elif args.player1 == "random":
-        action = random.randint(2 * width)
-    elif args.player1 == "human":
+        obs["goal"] = goal_board
+
+        done = False
+        step = 0
         print("      GOAL BOARD")
         print_board(goal_board)
-        print("Input move")
-        action = int(input()) - 1
-    print("      ", args.player1, " MOVE", action+1)
-    current_board = run(action, current_board, player=player)
-    print("      CURRENT BOARD")
-    print_board(current_board)
-    solveEnv = ShallowEnv(current_board, obs["goal"], step + 1, max_turns, len(current_board[0]), len(current_board), player)
-    # determine if goal is fulfilled
-    _, done = reward(solveEnv)
-    if done:
-        print("      ", args.player1, " WON")
-        break
-    player = -1
-    if args.player2 == "agent":
-        flipped_board = flip_board(deepcopy(current_board))
-        flipped_goal = flip_board(deepcopy(goal_board))
-        flipped_obs = OrderedDict()
-        flipped_obs["current"] = flipped_board
-        flipped_obs["goal"] = flipped_goal
-        paramEnv = ShallowEnv(flipped_board, flipped_goal, step, max_turns, len(current_board[0]), len(current_board), 1)
-        action = return_move(agent, paramEnv, flipped_obs)
-    elif args.player2 == "mcts":
-        action = mcts(current_board, int(args.mcts_depth), math.sqrt(2), goal_board, width*2, height, max_turns, step, player)
-    elif args.player2 == "random":
-        action = random.randint(2*width)
-    elif args.player2 == "human":
-        print("      GOAL BOARD")
-        print_board(goal_board)
-        print("Input move")
-        action = int(input())-1
-    print("      ", args.player2, " MOVE", action + 1)
-    current_board = run(action, current_board, player=player)
-    print("      CURRENT BOARD")
-    print_board(current_board)
-    solveEnv = ShallowEnv(current_board, obs["goal"], step + 1, max_turns, len(current_board[0]), len(current_board), player)
-    # determine if goal is fulfilled
-    _, done = reward(solveEnv)
-    if done:
-        print("      ", args.player2, " WON")
-        break
-    step+=1
-if not done:
-    print("      DRAW")
-print("    ", step, "MOVES")
+        print("      START BOARD")
+        print_board(current_board)
+        while step < max_turns:
+            print("      STEP", step+1)
+            player = 1
+            solveEnv = ShallowEnv(current_board, obs["goal"], step + 1, max_turns, len(current_board[0]), len(current_board),
+                                  player)
+            # determine if goal is fulfilled
+            _, done = reward(solveEnv)
+            if done:
+                if _ > 0:
+                    wonChallengesLvl += 1
+                    wonChallenges += 1
+                    print("     ", args.player1, "WON")
+                else:
+                    print("     ", args.player2, " WON")
+                print("      GOAL BOARD")
+                print_board(goal_board)
+                break
+            if args.player1 == "agent":
+                obs["current"] = current_board
+                paramEnv = ShallowEnv(current_board, obs["goal"], step, max_turns, len(current_board[0]), len(current_board), player)
+                action = return_move(agent, paramEnv, obs)
+            elif args.player1 == "mcts":
+                action = mcts(current_board, int(args.mcts_depth), math.sqrt(2), goal_board, width * 2, height, max_turns, step, player)
+            elif args.player1 == "random":
+                action = random.randint(2 * width)
+            elif args.player1 == "human":
+                print("      GOAL BOARD")
+                print_board(goal_board)
+                print("Input move")
+                action = int(input()) - 1
+            print("      ", args.player1, " MOVE", action+1)
+            current_board = run(action, current_board, player=player)
+            print("      CURRENT BOARD")
+            print_board(current_board)
+            solveEnv = ShallowEnv(current_board, obs["goal"], step + 1, max_turns, len(current_board[0]), len(current_board), player)
+            # determine if goal is fulfilled
+            _, done = reward(solveEnv)
+            if done:
+                wonChallengesLvl += 1
+                wonChallenges += 1
+                print("      ", args.player1, " WON")
+                print("      GOAL BOARD")
+                print_board(goal_board)
+                break
+            player = -1
+            if args.player2 == "agent":
+                flipped_board = flip_board(deepcopy(current_board))
+                flipped_goal = flip_board(deepcopy(goal_board))
+                flipped_obs = OrderedDict()
+                flipped_obs["current"] = flipped_board
+                flipped_obs["goal"] = flipped_goal
+                paramEnv = ShallowEnv(flipped_board, flipped_goal, step, max_turns, len(current_board[0]), len(current_board), 1)
+                action = return_move(agent, paramEnv, flipped_obs)
+            elif args.player2 == "mcts":
+                action = mcts(current_board, int(args.mcts_depth), math.sqrt(2), goal_board, width*2, height, max_turns, step, player)
+            elif args.player2 == "random":
+                action = random.randint(0, 2*width-1)
+            elif args.player2 == "human":
+                print("      GOAL BOARD")
+                print_board(goal_board)
+                print("Input move")
+                action = int(input())-1
+            print("      ", args.player2, " MOVE", action + 1)
+            current_board = run(action, current_board, player=player)
+            print("      CURRENT BOARD")
+            print_board(current_board)
+            solveEnv = ShallowEnv(current_board, obs["goal"], step + 1, max_turns, len(current_board[0]), len(current_board), player)
+            # determine if goal is fulfilled
+            _, done = reward(solveEnv)
+            if done:
+                print("      ", args.player2, " WON")
+                print("      GOAL BOARD")
+                print_board(goal_board)
+                break
+            step+=1
+        if not done:
+            print("      DRAW")
+        totalTurns += step
+        totalTurnsLvl += step
+        print("    ", step+1, "MOVES")
+    levelStats.append({"challenges": noOfChallengesLvl, "wonChallenges": wonChallengesLvl, "turns": totalTurnsLvl})
+
+print("\n\n----RESULTS----")
+for i in range(len(levelStats)):
+    print("LEVEL", i)
+    winrate = float(levelStats[i]["wonChallenges"])/float(levelStats[i]["challenges"])
+    avg_turns = float(levelStats[i]["turns"])/float(levelStats[i]["challenges"])
+    print("WINRATE:", winrate)
+    print("AVG TURNS:", avg_turns)
+print("TOTAL")
+winrate = float(wonChallenges)/float(noOfChallenges)
+avg_turns = float(totalTurns)/float(noOfChallenges)
+print("WINRATE:", winrate)
+print("AVG TURNS:", avg_turns)
