6ab444ce5262ecc80bd477ac69d7f05487db22cb
andre <ascheld@students.uni-mannheim.de>
2023-07-26 10:07:39 +0200

    temp

--- SecondHalf/frontend/react-avalanche/src/features/game/ts/Scenes/MainGame.ts
+++ SecondHalf/frontend/react-avalanche/src/features/game/ts/Scenes/MainGame.ts
@@ -314,7 +314,8 @@ export default class MainGame extends Phaser.Scene {
 			var onlinegame = this.gameMode as OnlineMultiPlayer;
 			onlinegame.gameOverEvent.on("gameOver", this.handleGameOver, this);
 			onlinegame.moveEvent.on("move", this.handleMove, this);
-			onlinegame.boardEvent.on("emit", this.getChallenge, this)
+			onlinegame.boardEvent.on("emit", this.getChallenge, this);
+			onlinegame.dcEvent.on("dc", this.testdc, this)
 		}
 
 		// Register the beforeupdate event
@@ -357,12 +358,14 @@ export default class MainGame extends Phaser.Scene {
 	  	console.log("Game Over!");
 	}
 
-
-
 	// Event handler for the "boardEvent"
 	private getChallenge() {
 		console.log("challenge recieved");
     }
+
+	private testdc(){
+		console.log("dc registered in maingame")
+	}
 
 	// Event handler for the "moveEvent"
 	private handleMove(col: number) {
